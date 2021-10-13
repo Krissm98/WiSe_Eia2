cb03c826dc24b2444d49f54decf2e650b170000f
Krissm98 <kriss.m985@gmail.com>
2021-10-13 15:18:38 +0200

    uebung1

---
+++ Vorlesung1/v.ts
@@ -0,0 +1,30 @@
+//let v: number = 1;
+//v = v + "1";
+//console.log(v);
+
+//let a = [7, true, "Hallo"];
+
+
+//let s = {"zahl": 7, "wahr": true, text: "Hallo"};
+//s[4] = [101, 102];
+//console.log(s); 
+
+interface VectorWithMeaning {
+    x: number;
+    y: number;
+    meaning: string;
+}
+let vector: VectorWithMeaning = {x: 12.4, y: -7.2, meaning: "Ortsvektor"};
+
+//console.log(vector.meaning);
+
+//function isDivisible(_dividend: number, _divisor: number): boolean {
+    //let result: boolean = (_dividend % _divisor == 0);
+    //return result;
+
+//}
+
+let v1: number = 8;
+let v2: number = v1;
+
+console.log(v1, v2);
